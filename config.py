705e9f241ee893d8251bf54b7e47c09426aaa7ce
bobur13013412 <53964721+bobur13013412@users.noreply.github.com>
2022-02-26 23:18:20 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+TOKEN = "5125014917:AAH3Vf4hsHJs3-zn34hWFHakyNfxu1h_r0g"
+
+CHANNEL_URL = "https://t.me/PYTHONDARSLARI22"
+CHANNEL_ID = "@PYTHONDARSLARI22"
+CHAT_ID = "@python_darlari"
+ADMIN_ID = "1632102384"
+
+WORDS = ['salom', 'qalaysan','группе','http','https','reklama',
+         'реклама','sotib','arzon','cум','cўм','som',"so'm",'сўм'
+
+
+
+         ]
